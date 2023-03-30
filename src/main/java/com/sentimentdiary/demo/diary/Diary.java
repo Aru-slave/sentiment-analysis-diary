89a82219ef3d7439f1cebe13b91e8d2542ea5f96
kung036 <sinariari3@gmail.com>
2023-03-30 21:42:41 +0900

    feat - #5 : 생성시간/생성날짜 클래스 추가 및 적용

--- src/main/java/com/sentimentdiary/demo/diary/Diary.java
+++ src/main/java/com/sentimentdiary/demo/diary/Diary.java
@@ -1,35 +1,39 @@
 package com.sentimentdiary.demo.diary;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sentimentdiary.demo.auditable.Auditable;
 import com.sentimentdiary.demo.member.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.util.*;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-public class Diary {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Diary extends Auditable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long diaryId;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String content;
-    @Column(nullable = false)
-    private LocalDateTime createDate = LocalDateTime.now();
-    @Column(nullable = false, name = "LAST_MODIFIED_AT")
-    private LocalDateTime modifiedAt = LocalDateTime.now();
 
+    @Column
+    private int emotion;
+
+    @Column
+    @ElementCollection
+    private Map<String, Integer> keywords = new HashMap<>();
 
     @JsonIgnore
     @ManyToOne(optional = false)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
-
 }
