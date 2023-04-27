3461f1610ffe43276c5e8268f93dfae82f49d804
Aru-slave <chlwlgus9584@naver.com>
2023-04-27 21:54:04 +0900

    #fix - 다이어리 조회 수정

--- src/main/java/com/sentimentdiary/demo/diary/DiaryRepository.java
+++ src/main/java/com/sentimentdiary/demo/diary/DiaryRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary,Long> {
     Optional<Diary> findById(Long diaryId);
+    Optional<Diary> findByCreatedAt(LocalDate createdAt);
     Page<Diary> findByMember(Member member, PageRequest pageRequest);
 
     List<Diary> findByMember(Member member);
