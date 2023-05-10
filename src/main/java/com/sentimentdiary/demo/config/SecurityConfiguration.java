09ecf50038091fda8566c1f07654ed5ce4dba4a7
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-05-10 16:27:08 +0900

    Update SecurityConfiguration.java

--- src/main/java/com/sentimentdiary/demo/config/SecurityConfiguration.java
+++ src/main/java/com/sentimentdiary/demo/config/SecurityConfiguration.java
@@ -89,8 +89,8 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("https://sentiment-analysis-diary.vercel.app", "http://localhost:3000", "https://localhost:3001", "https://localhost:3443"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setExposedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("x-auth-token", "Authorization", "Refresh", "heart-beat"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token", "Authorization", "Refresh", "heart-beat"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
